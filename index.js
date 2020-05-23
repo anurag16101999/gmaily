e1fa88de7ee65488d76b488368bbcf7c29b26933
Anurag Gupta <guptaanurag1610@gmail.com>
2020-05-23 19:56:01 +0530

    google authentication

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
+const keys = require("./config/keys");
 
-app.get("/", function (req, res) {
-  res.send("started");
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    //after authentication successful
+    // access token for doing things on users behalf like reading their mails
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken);
+      console.log(profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+//passport here is taking code send by in url and sending it to take the details
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
